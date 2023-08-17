c3b5ad439d1b363250da570b38b427c22f03475b
Abdikarim Hayow <abdikarim.hayow@outlook.com>
2023-08-17 15:52:49 -0500

    add a new file 'Topic.swift' to define the 'Topic' enum

---
+++ QuizThySelf/Models/Topic.swift
@@ -0,0 +1,18 @@
+//
+//  Topic.swift
+//  QuizThySelf
+//
+//  Created by Abdikarim Hayow on 8/17/23
+//  
+//
+
+
+
+
+
+enum Topic: String, CaseIterable {
+    case geography
+    case mythology
+    case religion
+    case history
+}
