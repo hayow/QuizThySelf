0621f0f1839d7f5a7b9fc27e56cbee4fa2be04ac
Abdikarim Hayow <abdikarim.hayow@outlook.com>
2023-08-18 16:05:42 -0500

    add a new file 'Difficulty.swift' to define the 'Difficulty' enum

---
+++ QuizThySelf/Models/Difficulty.swift
@@ -0,0 +1,17 @@
+//
+//  Difficulty.swift
+//  QuizThySelf
+//
+//  Created by Abdikarim Hayow on 8/18/23
+//  
+//
+
+
+
+
+
+enum Difficulty: String, CaseIterable {
+    case easy
+    case medium
+    case hard
+}
