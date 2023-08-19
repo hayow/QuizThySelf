4ca308e91fad43f968ea12610b655af5974df135
Abdikarim Hayow <abdikarim.hayow@outlook.com>
2023-08-19 11:37:57 -0500

    add a new file 'QuestionState.swift' to define the 'QuestionState' enum

---
+++ QuizThySelf/Models/QuestionState.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionState.swift
+//  QuizThySelf
+//
+//  Created by Abdikarim Hayow on 8/19/23
+//  
+//
+
+
+
+
+
+enum QuestionState {
+    case notAnswered
+    case answeredCorrectly
+    case answeredIncorrectly
+}
