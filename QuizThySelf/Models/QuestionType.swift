5b283ab52caa5b1c9a3ab9e10de590e09d8c77e9
Abdikarim Hayow <abdikarim.hayow@outlook.com>
2023-08-19 09:52:18 -0500

    add a new file 'QuestionType.swift' to define the 'QuestionType' enum

---
+++ QuizThySelf/Models/QuestionType.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionType.swift
+//  QuizThySelf
+//
+//  Created by Abdikarim Hayow on 8/19/23
+//  
+//
+
+
+
+
+
+enum QuestionType {
+    case trueOrFalse
+    case singleResponse
+    case multipleResponse(Int)
+}
